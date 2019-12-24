320aa02486f199f70a217b4048d60b84f2b29dc4
Tran Mach So Han <maklisa2000@gmail.com>
2019-12-24 21:51:47 +0700

    piece model with some basic features

---
+++ src/sample/piece/Position.java
@@ -0,0 +1,21 @@
+package sample.piece;
+
+public class Position {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setPosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
