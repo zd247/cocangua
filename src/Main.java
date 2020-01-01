0abda8c30c8b5ddebadca141e656a59fc6c02242
mnskgrayce <s3751450@rmit.edu.vn>
2020-01-01 23:25:00 +0700

    changed view (now in model). Added class Space (the circle a Piece will travel through) and Board (a board with HashMap of Spaces for getPositionByIndex functions). Home path not added. HashMap(0) is Blue's arrival position (marked)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,34 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import model.Board;
+
+import java.util.HashMap;
+
+import static javafx.scene.paint.Color.*;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
-        primaryStage.setTitle("Co Ca Ngua");
-        primaryStage.setScene(new Scene(root, 600 , 600));
+    public void start(Stage primaryStage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
+//        primaryStage.setTitle("Co Ca Ngua");
+//        primaryStage.setScene(new Scene(root, 1200 , 900));
+//        primaryStage.show();
+
+        // Draw board with circles of 4 colors
+        Board board = new Board();
+
+        // Scene stuff
+        primaryStage.setScene(new Scene(board, 800, 800));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
