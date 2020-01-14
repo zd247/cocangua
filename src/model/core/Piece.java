668a7fc0b7988c834b77c82ca0639336bd29008a
s3740819 <s3740819@rmit.edu.vn>
2020-01-14 23:21:49 +0700

    Header adding

--- src/model/core/Piece.java
+++ src/model/core/Piece.java
@@ -1,3 +1,24 @@
+/*
+  RMIT University Vietnam
+  Course: INTE2512 Object-Oriented Programming
+  Semester: 2020A
+  Assessment: Final Project
+  Created date: 20/12/2019
+
+  By:
+  Phan Quoc Binh (3715271)
+  Tran Mach So Han (3750789)
+  Tran Kim Bao (3740819)
+  Nguyen Huu Duy (3703336)
+  Nguyen Minh Trang (3751450)
+
+  Last modified: 14/1/2019
+
+  By:
+  Nguyen Huu Duy (3703336)
+
+  Acknowledgement: see readme.md
+*/
 package model.core;
 
 import javafx.animation.*;
